7b87ba7d86d9d6090fef79eb765916598d82c698
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-03-26 21:09:18 +0530

    Time: 3 ms (44.38%), Space: 7.2 MB (42.75%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(), salary.end());
+        double total = accumulate(salary.begin() + 1, salary.end() - 1, 0);
+        
+        return total / (salary.size() - 2);
+        
+    }
+};
