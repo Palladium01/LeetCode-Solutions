6e2188c859faf1128196df4dbef21956a67295e4
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-03-27 10:12:06 +0530

    Time: 2 ms (33.98%), Space: 5.8 MB (71.30%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int product=1;
+        int sum=0;
+        
+        while(n)
+        {
+            int rem=n%10;
+            product*=rem;
+            sum+=rem;
+            n=n/10;
+        }
+        
+        return product-sum;
+    }
+};
