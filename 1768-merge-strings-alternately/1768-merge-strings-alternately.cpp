95ea3ced0e3451a8594ab7700099ac0d037cc672
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-04-02 10:50:01 +0530

    Time: 2 ms (49.47%), Space: 6.3 MB (54.84%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0, j=0;
+        string res="";
+        while(i<word1.size()||j<word2.size())
+        {
+            if(i<word1.size())
+                res.push_back(word1[i++]);
+            if(j<word2.size())
+                res.push_back(word2[j++]);
+        }
+        
+        return res;
+    }
+};
