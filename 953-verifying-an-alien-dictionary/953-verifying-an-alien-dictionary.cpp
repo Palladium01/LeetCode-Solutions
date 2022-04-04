86c902f26f6972328f28af4e5606e939ef791a86
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-04-04 18:58:37 +0530

    Time: 9 ms (33.65%), Space: 9.6 MB (35.94%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+        unordered_map<char,int> m;
+    bool isAlienSorted(vector<string>& words, string order) {
+        
+        for(int i=0;i<order.size();++i)
+        {
+            m[order[i]]=i;
+        }
+        
+        for(int i=0;i<words.size()-1;++i)
+        {
+            if(!check(words[i],words[i+1]))
+                return false;
+        }
+        return true;
+    }
+    
+    
+    bool check(string &a, string &b)
+    {
+        int i=0;
+        
+        while(i<a.size()&&i<b.size())
+        {
+            if(a[i]!=b[i])
+                return m[a[i]]<m[b[i]];
+            i++;
+        }
+        
+        return a.size()<=b.size();
+    }
+};
