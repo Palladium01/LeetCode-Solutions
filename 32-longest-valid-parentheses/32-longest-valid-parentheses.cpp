0c83308c5ad44a7edde16a8079f56876a4591a7d
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-06-29 23:42:10 +0530

    Time: 4 ms (74.83%), Space: 7.3 MB (66.24%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int len=0;
+        stack<int> st;
+        
+        st.push(-1);
+        
+        for(int i=0;i<s.size();++i)
+        {
+            if(s[i]=='(')
+                st.push(i);
+            
+            else
+            {
+                st.pop();
+                if(st.empty())
+                    st.push(i);
+                else
+                    len=max(len,i-st.top());
+            }
+        }
+        
+        return len;
+    }
+};
