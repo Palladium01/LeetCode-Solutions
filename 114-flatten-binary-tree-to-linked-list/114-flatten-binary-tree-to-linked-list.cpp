7d2fa164f490b1269dd72c8c0ef9ee7498513f93
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-07-27 22:13:39 +0530

    Time: 26 ms (6.36%), Space: 12.8 MB (18.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -18,12 +18,12 @@ public:
 			while (t->right)
 				t = t->right;
 			t->right = root->right;
-		}
+		    }
 
         if(root->left)
 		    root->right = root->left;
 		root->left = NULL;
 		root = root->right;
-	}
+	    }
     }
 };
