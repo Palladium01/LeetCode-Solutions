307df6ed79ab048d52638bbb9c1e65f1b7ad1eaa
Palladium01 <84331796+Palladium01@users.noreply.github.com>
2022-08-23 14:07:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (54.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,14 +1,55 @@
 class Solution {
 public:
     
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp (m, vector<int> (n,1));
+//     int solve(int i, int j, vector<vector<int>>& dp){
+//         if(i<0 || j<0) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         if(i==0 && j==0) return 1;
         
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//         int up=solve(i-1,j,dp);
+//         int left=solve(i,j-1,dp);
+        
+//         return dp[i][j]=up+left;
+//     }
+    
+    int uniquePaths(int m, int n) {
+        // vector<vector<int>> dp (m, vector<int>(n,0));
+        // return solve(m-1,n-1,dp);
+        
+//         dp[0][0]=1;
+        
+//         for(int i=0; i<m; ++i){
+//             for(int j=0; j<n; ++j){
+//                 if(i==0 && j==0) continue;
+//                 int up=0, left=0;
+                
+//                 if(i>0) up=dp[i-1][j];
+//                 if(j>0) left=dp[i][j-1];
+
+//                  dp[i][j]=up+left;
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+        
+        vector<int> prev(n,0) ;
+        //prev[0]=0;
+        
+        for(int i=0; i<m; ++i){
+            vector<int> cur(n,0);
+            for(int j=0; j<n; ++j){
+                if(i==0 && j==0) cur[j]=1;
+                else{
+                    int up=0, left=0;
+                
+                    if(i>0) up=prev[j];
+                    if(j>0) left=cur[j-1];
+                    cur[j]=up+left;
+                }   
             }
+            prev=cur;
         }
-        return dp[m - 1][n - 1];
+        
+        return prev[n-1];
     }
 };
